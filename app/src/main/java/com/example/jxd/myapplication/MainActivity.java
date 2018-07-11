10eac17aeac2407c10cef7c1b33d487eb831f630
WangBinDeGit <854459337@qq.com>
2018-07-11 08:53:49 +0800

    修改main

--- app/src/main/java/com/example/jxd/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jxd/myapplication/MainActivity.java
@@ -9,5 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_login);
+        initView();
+    }
+
+    private void initView() {
+
     }
 }
