cb3156d9ea4f54f7a4927060d99ab49fb34a6b80
uzilover2018 <442635369@qq.com>
2018-07-12 10:16:02 +0800

    no message

---
+++ app/src/main/java/com/example/jxd/myapplication/layout_ZckLogin.java
@@ -0,0 +1,13 @@
+package com.example.jxd.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class layout_ZckLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zck_login);
+    }
+}
